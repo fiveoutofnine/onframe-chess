6a4968e52bad52ed39c3339303e6a456858fa6e9
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-01-31 02:31:54 +0900

    chore: update

--- app/(frame)/play/page.tsx
+++ app/(frame)/play/page.tsx
@@ -8,11 +8,9 @@ export default function Page() {
       <head>
         <title>Onframe Chess</title>
         <meta property="og:title" content={title} />
-        {/* TODO */}
-        <meta property="og:image" content="https://fiveoutofnine.com/static/og/home.png" />
+        <meta property="og:image" content="https://onframe-chess.vercel.app/static/og/home.png" />
         <meta name="fc:frame" content="vNext" />
-        {/* TODO */}
-        <meta name="fc:frame:image" content="https://fiveoutofnine.com/static/og/home.png" />
+        <meta name="fc:frame:image" content="https://onframe-chess.vercel.app/static/og/home.png" />
         <meta
           name="fc:frame:post_url"
           content={`${process.env.BASE_URL}/api/move?state=${encodeURIComponent(STARTING_POSITION)}&page=0&init=1`}
